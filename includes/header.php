7937491cebecc880fa2a99274ab650af9f90153c
BaddyM <101480645+BaddyM@users.noreply.github.com>
2023-04-26 23:41:56 +0300

    Second Commit

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 
 $brandname = "Cornerstone H.S";
 
-$brandtitle = "CORNERSTONE HIGH SCHOOL";
+$brandtitle = "CORNERSTONE HIGH SCHOOL NANGABO";
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
