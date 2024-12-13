6e2bb6bef8ee0ac01440799cb2a2265f047dfbf1
BADDY MUZIK <101480645+BaddyM@users.noreply.github.com>
2024-12-14 02:58:19 +0300

    Initial Commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,102 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./global.css";
+import Link from "next/link";
+
+const brandName = "Cornerstone High School Nangabo"
+
+var linkLabels = [
+  "Home",
+  "About",
+  "Academics",
+  "Admission",
+  "School Life",
+  "Contact Us",
+];
+var links = [
+  "home",
+  "about",
+  "academics",
+  "admission",
+  "schoollife",
+  "contact",
+];
+var topHeaderLabel = ["News", "E-Learning", "Portal"];
+var topHeaderLinks = ["news", "elearning", "portal"];
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="top-header shadow-sm w-100 text-center p-1 rounded-1">
+          <div className="socials">
+            <button className="whatsapp-btn">
+              <i className="bi bi-whatsapp"></i>
+            </button>
+            <button className="facebook-btn">
+              <i className="bi bi-facebook"></i>
+            </button>
+            <button className="tiktok-btn">
+              <i className="bi bi-tiktok"></i>
+            </button>
+            <button className="instagram-btn">
+              <i className="bi bi-instagram"></i>
+            </button>
+          </div>
+          <div>
+            <ul className="top-header-links">
+              {topHeaderLabel.map((e) => (
+                <Link
+                  className="nav-link"
+                  href={topHeaderLinks[topHeaderLabel.indexOf(e)]}
+                >
+                  <li>{e}</li>
+                </Link>
+              ))}
+            </ul>
+          </div>
+          {/*--------- Other links ------------*/}
+        </div>
+        <nav className="navbar navbar-expand-sm bg-primary my-4">
+          <div className="container">
+            <a className="navbar-brand fw-bold h6 mb-0 text-white" href="#">
+              {brandName}
+            </a>
+            <button
+              className="navbar-toggler d-lg-none"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapsibleNavId"
+              aria-controls="collapsibleNavId"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavId">
+              <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                    {linkLabels.map((e)=><li className="nav-item"><Link className="nav-link" href={links[linkLabels.indexOf(e)]}>{e}</Link></li>)}
+              </ul>
+              <form className="d-flex my-2 my-lg-0">
+                <input
+                style={{outline:"none",boxShadow:"none"}}
+                  className="form-control me-sm-2"
+                  type="text"
+                  placeholder="Search"
+                />
+                <button
+                  className="btn btn-warning my-2 my-sm-0"
+                  type="submit"
+                >
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,105 @@
+"use client";
+import Image from "next/image";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { useEffect, useState } from "react";
+
+var carousel = [
+  "DSC01988.jpg",
+  "DSC02053.jpg",
+  "netball_team.jpg",
+  "prefects.jpg",
+];
+
+var messagesImages = [
+    "PHOTO-2024-04-05-11-38-04.png",
+    "PHOTO-2022-08-09-07-51-21.png",
+    "IMG-20241112-WA0105.jpg",
+    "PHOTO-2024-11-19-15-43-14.jpg",
+    "IMG-20241105-WA0216.jpg"
+];
+
+var messagesLabels = [
+    "Founding Director",
+    "Managing Director",
+    "Head Teacher",
+    "Deputy Headteacher",
+    "Director of Studies"
+]
+
+export default function Page() {
+  useEffect(() => {
+    let screenWidth = screen.width;
+
+    // init Swiper:
+    const swiper = new Swiper(".swiper", {
+      // configure Swiper to use modules
+      modules: [Navigation, Pagination],
+      slidesPerView: screenWidth <= 768 ? 1 : 3,
+      direction: "horizontal",
+      loop: true,
+      autoplay: {
+        delay: 3000,
+      },
+
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination",
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+    });
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <div className="swiper shadow-sm bg-white">
+        <div className="swiper-wrapper">
+          {carousel.map((e) => (
+            <div key={carousel.indexOf(e)} className="swiper-slide text-center">
+              <Image
+                width={400}
+                height={400}
+                alt={`Carousel_Image`}
+                src={require(`../../public/images/carousel/${e}`)}
+              ></Image>
+            </div>
+          ))}
+        </div>
+
+        <div className="swiper-pagination"></div>
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next"></div>
+
+        <div className="swiper-scrollbar"></div>
+      </div>
+      {/*---------------- Swiper ---------------------------*/}
+
+      <div className="bg-white row justify-content-start my-3 w-100">
+        {messagesLabels.map((e)=>(
+            <div key={messagesLabels.indexOf(e)} className="col-md-3 mb-4">
+                <div className="card shadow-sm border-0">
+                <Image width={'100%'} height={'400'} className="img-fluid text-center" objectFit="contain" alt={`${e}`} src={require(`../../public/images/messages/${messagesImages[messagesLabels.indexOf(e)]}`)} />
+                <div className="card-body">
+                    
+                </div>
+            </div>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+}
